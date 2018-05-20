f772c34492c3841f4f8ea55d77fa479ef8cf43a4
bahrouz <info@freelancemodule.com>
2018-05-20 17:47:14 +0200

    Video07 Services
    Video07 Services

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Psr\Log\LoggerInterface;
 
 class CourseController extends Controller
 {
     /**
      * @Route("/course", name="course")
      */
-    public function index()
-    {
+    public function index(LoggerInterface $logger) 
+    { 
+       $logger->info('This is a course');
         return $this->render('course/index.html.twig', [
             'controller_name' => 'CourseController',
         ]);
