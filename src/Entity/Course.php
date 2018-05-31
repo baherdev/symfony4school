179b3eb9ababc5329d81c961564ce93640f2451f
Baher Rais <baher.rais@gmail.com>
2018-05-31 12:54:48 +0200

    Video 08 Entity

---
+++ src/Entity/Course.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+class Course{
+
+  private $id;
+
+  private $title;
+
+  public function getId(){
+    return $this->id;
+  }
+
+  public function setId($id){
+    $this->id = $id;
+    return $this;
+  }
+
+  public function getTitle(){
+    return $this->title;
+  }
+
+  public function setTitle($title){
+    $this->title = $title;
+    return $this;
+  }
+
+}
---
+++ src/Entity/Grade.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GradeRepository")
+ */
+class Grade
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="decimal", precision=0)
+     */
+    private $year;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function setYear($year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+}
